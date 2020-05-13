02aa4112bc4cb8bd47bcaab3bcd26f2f775d20d6
superturboryan <forsyth.r@gmail.com>
2020-05-12 22:39:11 -0400

    Complete 'Queue to do'

--- Queue To Do/Solution.java
+++ Queue To Do/Solution.java
@@ -1,79 +1,80 @@
-/*
-===========
-Queue To Do
-===========
-You're almost ready to make your move to destroy the LAMBCHOP doomsday device, 
-but the security checkpoints that guard the underlying systems of the LAMBCHOP are going to be a problem. 
-
-You were able to take one down without tripping any alarms, which is great! 
-Except that as Commander Lambda's assistant, you've learned that the checkpoints are about to come under automated review, 
-which means that your sabotage will be discovered and your cover blown - unless you can trick the automated review system.
-
-To trick the system, you'll need to write a program to return the same security checksum that the guards would have after they would have checked all the workers through. 
-
-Fortunately, Commander Lambda's desire for efficiency won't allow for hours-long lines, so the checkpoint guards have found ways to quicken the pass-through rate. 
-
-Instead of checking each and every worker coming through, the guards instead go over everyone in line while noting their security IDs, 
-then allow the line to fill back up. Once they've done that they go over the line again, this time leaving off the last worker. 
-
-They continue doing this, leaving off one more worker from the line each time but recording the security IDs of those they do check, 
-until they skip the entire line, at which point they XOR the IDs of all the workers they noted into a checksum and then take off for lunch. 
-
-Fortunately, the workers' orderly nature causes them to always line up in numerical order without any gaps.
-
-For example, if the first worker in line has ID 0 and the security checkpoint line holds three workers, the process would look like this:
-0 1 2 /
-3 4 / 5
-6 / 7 8
-where the guards' XOR (^) checksum is 0^1^2^3^4^6 == 2.
-
-Likewise, if the first worker has ID 17 and the checkpoint holds four workers, the process would look like:
-17 18 19 20 /
-21 22 23 / 24
-25 26 / 27 28
-29 / 30 31 32
-which produces the checksum 17^18^19^20^21^22^23^25^26^29 == 14.
-
-All worker IDs (including the first worker) are between 0 and 2000000000 inclusive, and the checkpoint line will always be at least 1 worker long.
-
-With this information, write a function solution(start, length) that will cover for the missing security checkpoint 
-by outputting the same checksum the guards would normally submit before lunch. 
-
-You have just enough time to find out the ID of the first worker to be checked (start) and the length of the line (length) before the automatic review occurs, 
-so your program must generate the proper checksum with just those two values.
-
-Input:
-Solution.solution(0, 3)
-Output:
-    2
-
-Input:
-Solution.solution(17, 4)
-Output:
-    14
-*/
+import java.util.Arrays;
 
 /*
 Thoughts: 
 -There is a trick for getting the XOR of ranges faster than performing each operation, maybe a sub-problem of the bigger problem
-
-L=4
-17 18 19 20 /
-21 22 23 / 24
-25 26 / 27 28
-29 / 30 31 32
-which produces the checksum 17^18^19^20^21^22^23^25^26^29 == 14.
 */
 
 public class Solution {
     
     public static void main(String[] args) {
-
+        // int[][] ranges = getRangesToCheck(17, 5);
+        // System.out.println(Arrays.toString(ranges));
+        int answer = solution(0, 3);
+        System.out.printf("\nAnswer:%d \n",answer);
     }
 
     public static int solution(int start, int length) {
         
-        return 0;
+        // Get all ranges to be checked by helper
+        int[][] ranges = getRangesToCheck(start, length);
+        // Array to store XOR values of each line
+        int[] lineXors = new int[length];
+        // For each range...
+        int i = 0;
+        for (int[] range : ranges) {
+            // Get the range XOR value using helper
+            int rangeXor = getXorRange(range[0], range[1]);
+            // Add it to array
+            lineXors[i] = rangeXor;
+            i++;
+        }
+        
+        int answer = 0;
+        // Using the XOR values computed for each line
+        for (int num : lineXors) {
+            // XOR all the values together one last time
+            answer ^= num;
+        }
+        return answer;
+    }
+
+    // Returns a 2D array of two value array ranges [start,end] to be checked by range helper
+    public static int[][] getRangesToCheck(int start, int length) {
+
+        int[][] ranges = new int[length][2];
+
+        int lineLength = length * length;
+        int jump = length - 1;
+        int i = start;
+        int last = start + lineLength - length;
+        int lineCount = 0;
+
+        while(jump != 0) {
+            int[] newRange = new int[2];
+            // Find beginning and end of range to add based on jump to be taken
+            int beg = i;
+            int end = i + jump;
+            newRange[0] = beg;
+            newRange[1] = end;
+            // Add the new range to the ranges to be returned
+            ranges[lineCount] = newRange;
+            // System.out.println(Arrays.toString(ranges[lineCount]));
+            // Incrememnt to the beginning of the next range
+            i += jump + lineCount + 1;
+            jump -= 1;
+            lineCount += 1;
+        }
+        // Add a final range of length 1 for the last number
+        int[] lastRange = new int[2];
+        int beg = last;
+        int end = last;
+        lastRange[0] = beg;
+        lastRange[1] = end;
+        ranges[lineCount] = lastRange;
+        // System.out.println(Arrays.toString(ranges[lineCount]));
+        // Return constructed ranges array
+        return ranges;
     }
 
     public static int computeXOR(int n) {  
