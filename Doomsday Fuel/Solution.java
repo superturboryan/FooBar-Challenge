7bc6e79098fe510c688efdaee472af7b043f43ad
superturboryan <forsyth.r@gmail.com>
2020-05-14 00:40:00 -0400

    Continue doomsday fuel

--- Doomsday Fuel/Solution.java
+++ Doomsday Fuel/Solution.java
@@ -1,12 +1,15 @@
 /*
 Thoughts:
 -Find terminal levels
--Find probabilities for each terminal level one by one
--Find common denominator for all numbers
+-Init array to hold probability for each level
+-Going line by line, adjust probabilities for each level
 -Construct answer array
+
+-STOCHASTIC MATRIX + MARKOV CHAINS!
 */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
         
@@ -19,19 +22,94 @@ public class Solution {
             {0,0,0,0,0,0},  // s2 is terminal, and unreachable (never observed in practice)
             {0,0,0,0,0,0},  // s3 is terminal
             {0,0,0,0,0,0},  // s4 is terminal
-            {0,0,0,0,0,0}};  // s5 is terminal
-          
+            {0,0,0,0,0,0}}; // s5 is terminal
+            
+            // s2 has probability 0     = 0.000
+            // s3 has probability 3/14  = 0.214
+            // s4 has probability 1/7   = 0.142
+            // s5 has probability 9/14  = 0.643
+
         int[] answer = solution(input);
+        System.out.println("\n"+answer);
     }
 
     public static int[] solution(int[][] m) {
         
-        ArrayList terms = getTerminalLevels(m); 
+        ArrayList<Integer> levelSums = getLevelSums(m);
+        ArrayList<Integer> terminalLevels = getTerminalLevels(m); 
 
-        System.out.println("\n\n"+terms.toString());
+        System.out.println("\n\n"+levelSums.toString());
+        System.out.println("\n\n"+terminalLevels.toString());
         
-        int[] ret = new int[0];
+        double[] probabilityForEachLevel = new double[m.length];
+
+        // For each level
+        for (int i = 0; i < m.length; i++) {
+            
+            // If level is terminal, continue
+            if (terminalLevels.contains(i)) continue;
+            
+            // For each digit in level
+            for (int j = 0; j < m.length; j++) {
+                
+                int digit = m[i][j];
+                // Looking for digits that aren't 0...
+                if (digit == 0) continue;
+
+                // For the first row we can set prob for each level accordingly
+                if (i == 0) {
+
+                    // If target level is not terminal
+                    if (!terminalLevels.contains(j)) 
+                        continue;
+                    
+                    double totalProb = (double)digit / (double)levelSums.get(i);
+                    probabilityForEachLevel[j] = totalProb;
+                    continue; // Move to next digit
+                }
+
+                double totalProb = (((double)digit / (double)levelSums.get(i)) * (probabilityForEachLevel[i] == 0 ? 1 : probabilityForEachLevel[i]));
+
+                // Otherwise...
+                // Check if target level is before current level
+                // If so, go back through previous level with new multiplier, 
+                if (j <= i) {
+            
+                    for (int x = 0; x < m.length; x++) {
+                        
+                        int otherDig = m[j][x];
+
+                        // Skipping any numbers pointing to current number's row
+                        if (otherDig == 0) continue;
+                        
+                        double otherProb = ((double)otherDig / (double)levelSums.get(i));
+
+                        // probabilityForEachLevel[x] += otherProb;
+                    }
+                }
+                else {
+                    probabilityForEachLevel[j] += totalProb;
+                }
+            }
+        }
+
+        System.out.println("");
+        for (int i = 0; i < terminalLevels.size(); i++) {
+            System.out.printf("%.3f, ",probabilityForEachLevel[terminalLevels.get(i)]);
+        }
+        System.out.println("\n" + Arrays.stream(probabilityForEachLevel).sum());
+
+        int[] ret = {1};
         return ret;
+    }
+
+    // Convenience
+    public static ArrayList<Integer> getLevelSums(int[][] m) {
+        ArrayList<Integer> levelSums = new ArrayList<Integer>();
+        for (int[] level : m) {
+            levelSums.add(Arrays.stream(level).sum());
+        }
+        return levelSums;
     }
 
     public static ArrayList<Integer> getTerminalLevels(int[][] m) {
